4532029023fa5c989300d624024740ac785b8e77
Erich Purpur <epurpur@gmail.com>
2023-06-16 14:55:31 -0400

    scraping names from first review page

--- Trustpilot_webscrape.py
+++ Trustpilot_webscrape.py
@@ -1,2 +1,25 @@
 
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import pandas as pd
 
+url = "https://www.trustpilot.com/review/www.misfitsmarket.com"
+
+
+source = requests.get(url)
+print(source.text)
+soup = BeautifulSoup(source.content, 'html.parser')
+
+reviewer_names = soup.find_all('span', class_="typography_heading-xxs__QKBS8 typography_appearance-default__AAY17")
+#print(reviewer_names.text)
+
+for i in reviewer_names:
+    print(i.text)
+    print()
+    print()
+
+# for item in soup.find_all('div', class_="styles_consumerInfoWrapper__KP3Ra"):
+#     print(item)
+
+#styles_consumerInfoWrapper__KP3Ra
